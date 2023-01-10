96f6c33fb0236d1e2cd03a540486d57f0a6444fd
Sylvia Lutze <lutzes@gen-slurm-slogin-p01.cluster.ihme.washington.edu>
2023-01-10 08:08:44 -0800

    Adding updates to correlation

--- correlation.R
+++ correlation.R
@@ -27,118 +27,37 @@ colnames(data)
 #bmi - based on measurements of height/weight 
 #lbxglu - FPG values 
 
-sbp_ldl_ken = cor(data$bpsys, data$lbdldl, method = c("kendall"), use = "complete.obs")
-sbp_ldl_pear = cor(data$bpsys, data$lbdldl, method = c("pearson"), use = "complete.obs")
-sbp_ldl_spea = cor(data$bpsys, data$lbdldl, method = c("spearman"), use = "complete.obs")
-sbp_ldl_ken 
-sbp_ldl_pear
-sbp_ldl_spea
-ggscatter(data, x='bpsys', y='lbdldl', add = 'reg.line')
-
-sbp_bmi_ken = cor(data$bpsys, data$bmi, method = c("kendall"), use = "complete.obs")
-sbp_bmi_pear = cor(data$bpsys, data$bmi, method = c("pearson"), use = "complete.obs")
-sbp_bmi_spea = cor(data$bpsys, data$bmi, method = c("spearman"), use = "complete.obs")
-sbp_bmi_ken 
-sbp_bmi_pear
-sbp_bmi_spea
-ggscatter(data, x='bpsys', y='bmi', add = 'reg.line')
-
-ldl_bmi_ken = cor(data$lbdldl, data$bmi, method = c("kendall"), use = "complete.obs")
-ldl_bmi_pear = cor(data$lbdldl, data$bmi, method = c("pearson"), use = "complete.obs")
-ldl_bmi_spea = cor(data$lbdldl, data$bmi, method = c("spearman"), use = "complete.obs")
-ldl_bmi_ken 
-ldl_bmi_pear
-ldl_bmi_spea
-ggscatter(data, x='lbdldl', y='bmi', add = 'reg.line')
-
-sbp_fpg_spea = cor(data$bpsys, data$lbxglu, method = c("spearman"), use = "complete.obs")
-ldl_fpg_spea = cor(data$lbdldl, data$lbxglu, method = c("spearman"), use = "complete.obs")
-bmi_fpg_spea = cor(data$bmi, data$lbxglu, method = c("spearman"), use = "complete.obs")
-sbp_fpg_spea 
-ldl_fpg_spea
-bmi_fpg_spea
-ggscatter(data, x='bpsys', y='lbxglu', add = 'reg.line')
-
-data_20_25 = data[(data$age_year>20) & (data$age_year<25)]
-sbp_ldl_spea = cor(data_20_25$bpsys, data_20_25$lbdldl, method = c("spearman"), use = "complete.obs")
-sbp_ldl_spea
-sbp_bmi_spea = cor(data_20_25$bpsys, data_20_25$bmi, method = c("spearman"), use = "complete.obs")
-sbp_bmi_spea
-ldl_bmi_spea = cor(data_20_25$lbdldl, data_20_25$bmi, method = c("spearman"), use = "complete.obs")
-ldl_bmi_spea
-
-data_final <- data.frame(matrix(ncol=9,nrow=0))
-colnames(data_final) <- c('sex_id','age_start','age_end','sbp_ldl','sbp_bmi','ldl_bmi','sbp_fpg','ldl_fpg','bmi_fpg')
+#This is testing for correlation for the first time by age and includes p-values
+data_final_2 <- data.frame(matrix(ncol=14,nrow=0))
+colnames(data_final_2) <- c('age_start','age_end','sbp_ldl','sbp_ldl_pval','sbp_bmi','sbp_bmi_pval','ldl_bmi','ldl_bmi_pval','sbp_fpg','sbp_fpg_pval','ldl_fpg','ldl_fpg_pval','bmi_fpg','bmi_fpg_pval')
 counter = 1
 
-for (j in 1:2){
-  for (i in 1:15){
-    age_low = 25 + (5*(i-1))
-    age_high = age_low + 5 
-    data_current <- data2[(data2$age_year>age_low) & (data2$age_year<age_high) & (data2$sex_id==j)]
-    sbp_ldl_spea = cor(data_current$bpsys, data_current$lbdldl, method = c("spearman"), use = "na.or.complete")
-    sbp_bmi_spea = cor(data_current$bpsys, data_current$bmi, method = c("spearman"), use = "na.or.complete")
-    ldl_bmi_spea = cor(data_current$lbdldl, data_current$bmi, method = c("spearman"), use = "na.or.complete")
-    sbp_fpg_spea = cor(data_current$bpsys, data_current$lbxglu, method = c("spearman"), use = "na.or.complete")
-    ldl_fpg_spea = cor(data_current$lbdldl, data_current$lbxglu, method = c("spearman"), use = "na.or.complete")
-    bmi_fpg_spea = cor(data_current$bmi, data_current$lbxglu, method = c("spearman"), use = "na.or.complete")
-    data_final[counter,1] = j
-    data_final[counter,2] = age_low 
-    data_final[counter,3] = age_high
-    data_final[counter,4] = sbp_ldl_spea
-    data_final[counter,5] = sbp_bmi_spea
-    data_final[counter,6] = ldl_bmi_spea
-    data_final[counter,7] = sbp_fpg_spea
-    data_final[counter,8] = ldl_fpg_spea
-    data_final[counter,9] = bmi_fpg_spea
-    print(counter)
-    counter = counter+1
-  }
-  
-}
-
-data_final
-
-write.csv(data_final, 'H:/correlation.csv')
-
-
-value <- cor.test(data$bpsys, data$lbxglu, method = "spearman", alternative = "two.sided")
-value$p.value
-
-data_final_2 <- data.frame(matrix(ncol=15,nrow=0))
-colnames(data_final_2) <- c('sex_id','age_start','age_end','sbp_ldl','sbp_ldl_pval','sbp_bmi','sbp_bmi_pval','ldl_bmi','ldl_bmi_pval','sbp_fpg','sbp_fpg_pval','ldl_fpg','ldl_fpg_pval','bmi_fpg','bmi_fpg_pval')
-counter = 1
-
-for (j in 1:2){
-  for (i in 1:13){
-    age_low = 25 + (5*(i-1))
-    age_high = age_low + 5 
-    data_current <- data[(data$age_year>age_low) & (data$age_year<age_high) & (data$sex_id==j)]
-    sbp_ldl_spea = cor.test(data_current$bpsys, data_current$lbdldl, method = c("spearman"))
-    sbp_bmi_spea = cor.test(data_current$bpsys, data_current$bmi, method = c("spearman"))
-    ldl_bmi_spea = cor.test(data_current$lbdldl, data_current$bmi, method = c("spearman"))
-    sbp_fpg_spea = cor.test(data_current$bpsys, data_current$lbxglu, method = c("spearman"))
-    ldl_fpg_spea = cor.test(data_current$lbdldl, data_current$lbxglu, method = c("spearman"))
-    bmi_fpg_spea = cor.test(data_current$bmi, data_current$lbxglu, method = c("spearman"))
-    data_final_2[counter,1] = j
-    data_final_2[counter,2] = age_low 
-    data_final_2[counter,3] = age_high
-    data_final_2[counter,4] = sbp_ldl_spea$estimate
-    data_final_2[counter,5] = sbp_ldl_spea$p.value
-    data_final_2[counter,6] = sbp_bmi_spea$estimate
-    data_final_2[counter,7] = sbp_bmi_spea$p.value
-    data_final_2[counter,8] = ldl_bmi_spea$estimate
-    data_final_2[counter,9] = ldl_bmi_spea$p.value
-    data_final_2[counter,10] = sbp_fpg_spea$estimate
-    data_final_2[counter,11] = sbp_fpg_spea$p.value
-    data_final_2[counter,12] = ldl_fpg_spea$estimate
-    data_final_2[counter,13] = ldl_fpg_spea$p.value
-    data_final_2[counter,14] = bmi_fpg_spea$estimate
-    data_final_2[counter,15] = bmi_fpg_spea$p.value
-    print(counter)
-    counter = counter+1
-  }
-  
+for (i in 1:13){
+  age_low = 25 + (5*(i-1))
+  age_high = age_low + 5 
+  data_current <- data[(data$age_year>age_low) & (data$age_year<age_high)]
+  sbp_ldl_spea = cor.test(data_current$bpsys, data_current$lbdldl, method = c("spearman"))
+  sbp_bmi_spea = cor.test(data_current$bpsys, data_current$bmi, method = c("spearman"))
+  ldl_bmi_spea = cor.test(data_current$lbdldl, data_current$bmi, method = c("spearman"))
+  sbp_fpg_spea = cor.test(data_current$bpsys, data_current$lbxglu, method = c("spearman"))
+  ldl_fpg_spea = cor.test(data_current$lbdldl, data_current$lbxglu, method = c("spearman"))
+  bmi_fpg_spea = cor.test(data_current$bmi, data_current$lbxglu, method = c("spearman"))
+  data_final_2[counter,1] = age_low 
+  data_final_2[counter,2] = age_high
+  data_final_2[counter,3] = sbp_ldl_spea$estimate
+  data_final_2[counter,4] = sbp_ldl_spea$p.value
+  data_final_2[counter,5] = sbp_bmi_spea$estimate
+  data_final_2[counter,6] = sbp_bmi_spea$p.value
+  data_final_2[counter,7] = ldl_bmi_spea$estimate
+  data_final_2[counter,8] = ldl_bmi_spea$p.value
+  data_final_2[counter,9] = sbp_fpg_spea$estimate
+  data_final_2[counter,10] = sbp_fpg_spea$p.value
+  data_final_2[counter,11] = ldl_fpg_spea$estimate
+  data_final_2[counter,12] = ldl_fpg_spea$p.value
+  data_final_2[counter,13] = bmi_fpg_spea$estimate
+  data_final_2[counter,14] = bmi_fpg_spea$p.value
+  print(counter)
+  counter = counter+1
 }
 
 data_final_2
@@ -150,6 +69,9 @@ plot(data_final_2$age_start, data_final_2$sbp_fpg)
 plot(data_final_2$age_start, data_final_2$ldl_fpg)
 plot(data_final_2$age_start, data_final_2$bmi_fpg)
 
+#Next, we remove folks on treatment. This is since those on treatment 
+#might have a low SBP or LDL-C and throw off correlation 
+
 data[,sbptx:=ifelse(highbp==0 & is.na(bpmeds), 0, bpmeds)]
 data[,choltx:=ifelse(highchol==0 & is.na(cholmeds), 0, cholmeds)]
 data[,tx:=ifelse(sbptx==0 & choltx==0, "none", ifelse(sbptx==1 & choltx==0, "bponly",
@@ -157,40 +79,36 @@ data[,tx:=ifelse(sbptx==0 & choltx==0, "none", ifelse(sbptx==1 & choltx==0, "bpo
 data[,tx2:=factor(tx, levels=c("none", "bponly", "cholonly", "both"))]
 data2 <- data[data$tx2 == 'none']
 
-data_final_3 <- data.frame(matrix(ncol=15,nrow=0))
-colnames(data_final_3) <- c('sex_id','age_start','age_end','sbp_ldl','sbp_ldl_pval','sbp_bmi','sbp_bmi_pval','ldl_bmi','ldl_bmi_pval','sbp_fpg','sbp_fpg_pval','ldl_fpg','ldl_fpg_pval','bmi_fpg','bmi_fpg_pval')
+data_final_3 <- data.frame(matrix(ncol=14,nrow=0))
+colnames(data_final_3) <- c('age_start','age_end','sbp_ldl','sbp_ldl_pval','sbp_bmi','sbp_bmi_pval','ldl_bmi','ldl_bmi_pval','sbp_fpg','sbp_fpg_pval','ldl_fpg','ldl_fpg_pval','bmi_fpg','bmi_fpg_pval')
 counter = 1
 
-for (j in 1:2){
-  for (i in 1:13){
-    age_low = 25 + (5*(i-1))
-    age_high = age_low + 5 
-    data_current <- data2[(data2$age_year>age_low) & (data2$age_year<age_high) & (data2$sex_id==j)]
-    sbp_ldl_spea = cor.test(data_current$bpsys, data_current$lbdldl, method = c("spearman"))
-    sbp_bmi_spea = cor.test(data_current$bpsys, data_current$bmi, method = c("spearman"))
-    ldl_bmi_spea = cor.test(data_current$lbdldl, data_current$bmi, method = c("spearman"))
-    sbp_fpg_spea = cor.test(data_current$bpsys, data_current$lbxglu, method = c("spearman"))
-    ldl_fpg_spea = cor.test(data_current$lbdldl, data_current$lbxglu, method = c("spearman"))
-    bmi_fpg_spea = cor.test(data_current$bmi, data_current$lbxglu, method = c("spearman"))
-    data_final_3[counter,1] = j
-    data_final_3[counter,2] = age_low 
-    data_final_3[counter,3] = age_high
-    data_final_3[counter,4] = sbp_ldl_spea$estimate
-    data_final_3[counter,5] = sbp_ldl_spea$p.value
-    data_final_3[counter,6] = sbp_bmi_spea$estimate
-    data_final_3[counter,7] = sbp_bmi_spea$p.value
-    data_final_3[counter,8] = ldl_bmi_spea$estimate
-    data_final_3[counter,9] = ldl_bmi_spea$p.value
-    data_final_3[counter,10] = sbp_fpg_spea$estimate
-    data_final_3[counter,11] = sbp_fpg_spea$p.value
-    data_final_3[counter,12] = ldl_fpg_spea$estimate
-    data_final_3[counter,13] = ldl_fpg_spea$p.value
-    data_final_3[counter,14] = bmi_fpg_spea$estimate
-    data_final_3[counter,15] = bmi_fpg_spea$p.value
-    print(counter)
-    counter = counter+1
-  }
-  
+for (i in 1:13){
+  age_low = 25 + (5*(i-1))
+  age_high = age_low + 5 
+  data_current <- data2[(data2$age_year>age_low) & (data2$age_year<age_high)]
+  sbp_ldl_spea = cor.test(data_current$bpsys, data_current$lbdldl, method = c("spearman"))
+  sbp_bmi_spea = cor.test(data_current$bpsys, data_current$bmi, method = c("spearman"))
+  ldl_bmi_spea = cor.test(data_current$lbdldl, data_current$bmi, method = c("spearman"))
+  sbp_fpg_spea = cor.test(data_current$bpsys, data_current$lbxglu, method = c("spearman"))
+  ldl_fpg_spea = cor.test(data_current$lbdldl, data_current$lbxglu, method = c("spearman"))
+  bmi_fpg_spea = cor.test(data_current$bmi, data_current$lbxglu, method = c("spearman"))
+  data_final_3[counter,1] = age_low 
+  data_final_3[counter,2] = age_high
+  data_final_3[counter,3] = sbp_ldl_spea$estimate
+  data_final_3[counter,4] = sbp_ldl_spea$p.value
+  data_final_3[counter,5] = sbp_bmi_spea$estimate
+  data_final_3[counter,6] = sbp_bmi_spea$p.value
+  data_final_3[counter,7] = ldl_bmi_spea$estimate
+  data_final_3[counter,8] = ldl_bmi_spea$p.value
+  data_final_3[counter,9] = sbp_fpg_spea$estimate
+  data_final_3[counter,10] = sbp_fpg_spea$p.value
+  data_final_3[counter,11] = ldl_fpg_spea$estimate
+  data_final_3[counter,12] = ldl_fpg_spea$p.value
+  data_final_3[counter,13] = bmi_fpg_spea$estimate
+  data_final_3[counter,14] = bmi_fpg_spea$p.value
+  print(counter)
+  counter = counter+1
 }
 
 data_final_3
@@ -202,9 +120,99 @@ plot(data_final_3$age_start, data_final_3$sbp_fpg) #Slower decrease
 plot(data_final_3$age_start, data_final_3$ldl_fpg) #High in young people, 0 by 70
 plot(data_final_3$age_start, data_final_3$bmi_fpg) #Consistent for all ages 
 
+#Plots for Greg 
+plot(data_final_3$age_start, data_final_3$sbp_bmi, main='SBP and BMI Correlation') 
+loessMod10 <- loess(sbp_bmi ~ age_start, data=data_final_3)
+j <- order(data_final_3$age_start)
+lines(data_final_3$age_start[j],loessMod10$fitted[j],col="red",lwd=3)
+abline(h=0)
+
+plot(data_final_3$age_start, data_final_3$sbp_ldl, main='SBP and LDL-C Correlation') 
+loessMod10 <- loess(sbp_ldl ~ age_start, data=data_final_3)
+j <- order(data_final_3$age_start)
+lines(data_final_3$age_start[j],loessMod10$fitted[j],col="red",lwd=3)
+abline(h=0)
+
+plot(data_final_3$age_start, data_final_3$ldl_bmi, main='LDL-C and BMI Correlation') 
+loessMod10 <- loess(ldl_bmi ~ age_start, data=data_final_3)
+j <- order(data_final_3$age_start)
+lines(data_final_3$age_start[j],loessMod10$fitted[j],col="red",lwd=3)
+abline(h=0)
+
+plot(data_final_3$age_start, data_final_3$sbp_fpg, main='SBP and FPG Correlation') 
+loessMod10 <- loess(sbp_fpg ~ age_start, data=data_final_3)
+j <- order(data_final_3$age_start)
+lines(data_final_3$age_start[j],loessMod10$fitted[j],col="red",lwd=3)
+abline(h=0)
+
+plot(data_final_3$age_start, data_final_3$ldl_fpg, main='LDL-C and FPG Correlation') 
+loessMod10 <- loess(ldl_fpg ~ age_start, data=data_final_3)
+j <- order(data_final_3$age_start)
+lines(data_final_3$age_start[j],loessMod10$fitted[j],col="red",lwd=3)
+abline(h=0)
+
+plot(data_final_3$age_start, data_final_3$bmi_fpg, main='FPG and BMI Correlation') 
+loessMod10 <- loess(bmi_fpg ~ age_start, data=data_final_3)
+j <- order(data_final_3$age_start)
+lines(data_final_3$age_start[j],loessMod10$fitted[j],col="red",lwd=3)
+abline(h=0)
+
+#Based on a conversation with Greg, we agreed on the following: 
+#SBP and BMI correlation will be done to include age specific correlation 
+#SBP/LDL-C, SBP/FPG, LDL-C/BMI, and FPG/BMI will NOT include age specific 
+#correlation and instead will just be an overall value for everyone 
+#LDL-C/FPG correlation will be set to 1 
+
+#The below loop is adjusted for that new decision 
+
+data_final_5 <- data.frame(matrix(ncol=14,nrow=0))
+colnames(data_final_5) <- c('age_start','age_end','sbp_ldl','sbp_ldl_pval','sbp_bmi','sbp_bmi_pval','ldl_bmi','ldl_bmi_pval','sbp_fpg','sbp_fpg_pval','ldl_fpg','ldl_fpg_pval','bmi_fpg','bmi_fpg_pval')
+counter = 1
+
+for (i in 1:13){
+  age_low = 25 + (5*(i-1))
+  age_high = age_low + 5 
+  data_current <- data2[(data2$age_year>age_low) & (data2$age_year<age_high)]
+  sbp_ldl_spea = cor.test(data2$bpsys, data2$lbdldl, method = c("spearman"))
+  sbp_bmi_spea = cor.test(data_current$bpsys, data_current$bmi, method = c("spearman"))
+  ldl_bmi_spea = cor.test(data2$lbdldl, data2$bmi, method = c("spearman"))
+  sbp_fpg_spea = cor.test(data2$bpsys, data2$lbxglu, method = c("spearman"))
+  ldl_fpg_spea = 0
+  bmi_fpg_spea = cor.test(data2$bmi, data2$lbxglu, method = c("spearman"))
+  data_final_5[counter,1] = age_low 
+  data_final_5[counter,2] = age_high
+  data_final_5[counter,3] = sbp_ldl_spea$estimate
+  data_final_5[counter,4] = sbp_ldl_spea$p.value
+  data_final_5[counter,5] = sbp_bmi_spea$estimate
+  data_final_5[counter,6] = sbp_bmi_spea$p.value
+  data_final_5[counter,7] = ldl_bmi_spea$estimate
+  data_final_5[counter,8] = ldl_bmi_spea$p.value
+  data_final_5[counter,9] = sbp_fpg_spea$estimate
+  data_final_5[counter,10] = sbp_fpg_spea$p.value
+  data_final_5[counter,11] = 0
+  data_final_5[counter,12] = 'N/A'
+  data_final_5[counter,13] = bmi_fpg_spea$estimate
+  data_final_5[counter,14] = bmi_fpg_spea$p.value
+  print(counter)
+  counter = counter+1
+}
+
+data_final_5
+
+#Looks like the loop worked as expected! Now the last step is to reset 
+#the negatives in SBP/BMI to 0 as these are not statistically significant 
+#and the negatives are likely just due to noisy data 
+
+data_final_5[data_final_5<0] <- 0
+
+write.csv(data_final_5, 'H:/correlation.csv')
+
+#The below code is for a by year analysis that did not show any significant changes 
+#for different years 
 
 data <- data[,year2:=factor(svyyear, labels=c(1,2,3,4,5,6,7,8,9,10,11,12))]
-data2 <- data[data$tx2 == 'none']
+data2 <- data[(data$tx2 == 'none') | (data$tx2 == 'bponly')]
+data2 <- data[(data$tx2 == 'none')]
 
 data_final_4 <- data.frame(matrix(ncol=16,nrow=0))
 colnames(data_final_4) <- c('year','sex_id','age_start','age_end','sbp_ldl','sbp_ldl_pval','sbp_bmi','sbp_bmi_pval','ldl_bmi','ldl_bmi_pval','sbp_fpg','sbp_fpg_pval','ldl_fpg','ldl_fpg_pval','bmi_fpg','bmi_fpg_pval')
@@ -278,3 +286,7 @@ plot(data_final_4$year, data_final_4$ldl_bmi) #High for young, low by 50
 plot(data_final_4$year, data_final_4$sbp_fpg) #Slower decrease 
 plot(data_final_4$year, data_final_4$ldl_fpg) #High in young people, 0 by 70
 plot(data_final_4$year, data_final_4$bmi_fpg) #Consistent for all ages 
+
+
+
+
